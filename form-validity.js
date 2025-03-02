bfdcd885b7d3e04babe01e40b9f599d7f074eb8b
Aditi Dutta <duttaaditi37@gmail.com>
2025-03-02 10:40:18 -0800

    add character count

--- form-validity.js
+++ form-validity.js
@@ -3,13 +3,14 @@ let pattern = /^[a-zA-Z0-9 .,?!]*$/;
 let name = document.getElementById("name");
 let email = document.getElementById("email");
 let comments = document.getElementById("comments");
+let commentCount = document.getElementById("comment-count");
 
 let error = document.getElementById("error");
 
 comments.addEventListener("input", function (event) {
     if (!pattern.test(comments.value)) {
         comments.classList.add('flash-indicator');
-        comments.setCustomValidity("Non-typical characters are not allowed in comments.");
+        // comments.setCustomValidity("Non-typical characters are not allowed in comments.");
         error.value = "Error: you have typed an illegal character in comments!";
         setTimeout(() => {
             comments.classList.remove('flash-indicator');
@@ -17,3 +18,24 @@ comments.addEventListener("input", function (event) {
         }, '3000');
     }
 })
+
+comments.addEventListener('input', function() {
+    let text = comments.value;
+    const count = text.length;
+    commentCount.textContent = `Character count: ${count}`;
+
+    if (count > 450) {
+        comments.classList.add('text-limit');
+    }
+
+    if (count < 450) {
+        comments.classList.remove('text-limit');
+    }
+
+    if (count > 480) {
+        comments.classList.add('text-final');
+    }
+    if (count < 480) {
+        comments.classList.remove('text-final');
+    }
+  });
